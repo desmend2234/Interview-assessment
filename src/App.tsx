5e5c36ba2fa5c0d0a3e75a4c3fd0b6afdf6d2734
李季興 <jamesli@lijixingdeMacBook-Pro.local>
2025-02-21 19:36:06 +0800

    Initialize project structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center h-screen'>
+        <h1 className='text-4xl font-bold'>Interview assessment</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
